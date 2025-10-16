d8c8c2ade047dd9fb4ff2424a33d20bd7b187c11
Deepna John <deepnajohn4@gmail.com>
2025-10-16 17:12:07 +1300

    Fix JUnit: remove AppTest, add SimpleTest, update pom.xml

---
+++ src/test/java/edu/masseyuniversity/cinema/SimpleTest.java
@@ -0,0 +1,13 @@
+package edu.masseyuniversity.cinema;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SimpleTest {
+
+    @Test
+    void testAddition() {
+        assertEquals(2, 1 + 1, "Basic math should work");
+    }
+}
+
